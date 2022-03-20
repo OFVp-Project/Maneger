38c3442fc8e6ae00e4a04be3042d45ac858dae57
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-03-20 14:50:11 -0300

    Update Action Test

--- test/users/v3.js
+++ test/users/v3.js
@@ -1,7 +1,7 @@
 const http_requests = require("../http_request");
 
 async function CreateRequest(Username){
-  const User = await http_requests.postBuffer("http://localhost:3000/users/v3", {
+  const User = await http_requests.postBuffer(`${process.env.DAEMON_HOST||"http://localhost:3000"}/users/v3`, {
     username: Username,
     password: "aaaaaaaa14",
     date_to_expire: new Date((new Date().getTime()+(1000*60*60*7*30*2))).toString(),
@@ -9,12 +9,12 @@ async function CreateRequest(Username){
     wireguard_peers: 1
   }).then(res => JSON.parse(res.data.toString("utf8")));
   await Promise.all([
-    await http_requests.getBuffer(`http://localhost:3000/users/v3/Wireguard/json/${User.username}`),
-    await http_requests.getBuffer(`http://localhost:3000/users/v3/Wireguard/yaml/${User.username}`),
-    await http_requests.getBuffer(`http://localhost:3000/users/v3/Wireguard/wireguard/${User.username}`),
-    await http_requests.getBuffer(`http://localhost:3000/users/v3/Wireguard/openwrt18/${User.username}`)
+    await http_requests.getBuffer(`${process.env.DAEMON_HOST||"http://localhost:3000"}/users/v3/Wireguard/json/${User.username}`),
+    await http_requests.getBuffer(`${process.env.DAEMON_HOST||"http://localhost:3000"}/users/v3/Wireguard/yaml/${User.username}`),
+    await http_requests.getBuffer(`${process.env.DAEMON_HOST||"http://localhost:3000"}/users/v3/Wireguard/wireguard/${User.username}`),
+    await http_requests.getBuffer(`${process.env.DAEMON_HOST||"http://localhost:3000"}/users/v3/Wireguard/openwrt18/${User.username}`)
   ]);
-  await http_requests.postBuffer("http://localhost:3000/users/v3/delete", {username: User.username});
+  await http_requests.postBuffer(`${process.env.DAEMON_HOST||"http://localhost:3000"}/users/v3/delete`, {username: User.username});
   return User;
 }
 
