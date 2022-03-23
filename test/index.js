2bc49654e4206687cb45e83497fcf85eb23a6bf1
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-03-23 11:21:42 -0300

    Update Test

--- test/index.js
+++ test/index.js
@@ -1,12 +1,4 @@
 const crypto = require("crypto");
-(() => {
-  const RandomUsers = [];
-  for (let req = 0; req < parseInt(process.env.REQUESTS||5); req++) {
-    let idAdd = crypto.randomUUID().replace(/\-/gi, "").slice(0, 23);
-    console.log("Testing with username:", idAdd);
-    RandomUsers.push(require("./users/v1").main(idAdd).catch(err => {
-      console.log(err);
-      process.exit(1);
-    }));
-  }
+(async () => {
+  for (let req = 0; req < parseInt(process.env.REQUESTS||5); req++) await (require("./users/v1")).main(crypto.randomUUID().replace(/\-/gi, "").slice(0, 23));
 })();
