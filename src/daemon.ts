58309b1d4d73bfa497e015400aeac7660ca3572e
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-06-16 18:04:59 +0000

    Fix daemon

--- src/daemon.ts
+++ src/daemon.ts
@@ -22,19 +22,19 @@ io.on("connection", (socket) => {
   // Wireguard
   // Send Wireguard Config on Request
   socket.on("wireguardServerConfig", () => {
-    if (process.env.SHOWDAEMONLOG ==== "true") console.log("[Daemon]  Requested Wireguard Server Config");
+    if (process.env.SHOWDAEMONLOG === "true") console.log("[Daemon]  Requested Wireguard Server Config");
     return wireguard.wireguardInterfaceConfig().then(data => socket.emit("wireguardServerConfig", data));
   });
   // Send Wireguard users on request
   socket.on("wireguardUsers", () => {
-    if (process.env.SHOWDAEMONLOG ==== "true") console.log("[Daemon]  Requested Wireguard Users");
+    if (process.env.SHOWDAEMONLOG === "true") console.log("[Daemon]  Requested Wireguard Users");
     return wireguard.getUsers().then(data => socket.emit("wireguardUsers", data));
   });
 
   // OpenSSH
   // Send OpenSSH users on request
   socket.on("opensshUsers", () => {
-    if (process.env.SHOWDAEMONLOG ==== "true") console.log("[Daemon] Requested OpenSSH Users (Passwords is encrypted)");
+    if (process.env.SHOWDAEMONLOG === "true") console.log("[Daemon] Requested OpenSSH Users (Passwords is encrypted)");
     return ssh.getUsers().then(data => socket.emit("opensshUsers", data));
   });
 });
