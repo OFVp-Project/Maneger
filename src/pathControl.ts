eabdad0340d175bcbad592c8bc8ad13b4977d851
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-06-17 17:30:26 +0000

    update vscode extensions and dependabot

--- src/pathControl.ts
+++ src/pathControl.ts
@@ -1,3 +1,3 @@
-export const isDebug = (process.env.NODE_ENV === "development"||process.env.NODE_ENV === "testing");
+export const isDebug = (process.env.NODE_ENV === "development");
 export const onStorage = isDebug ? process.cwd() : "/data";
 export const emailValidate = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
