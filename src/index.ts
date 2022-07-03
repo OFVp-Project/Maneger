accd416f9141a49582fb6e290a7d493af068b924
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-07-03 03:11:30 +0000

    roll back

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import "./consoleLogColor";
 import * as API from "./api/index";
 import * as MongoConnect from "./mongo";
 import * as daemon from "./daemon";
-import * as newDaemon from "./socketDaemon";
 
 // Extends Process envs
 declare global {
@@ -46,8 +45,7 @@ declare global {
 console.log("Connecting in mongo database");
 MongoConnect.ConnectionStatus().then(() => console.log("Sucess to connect in database"))
 // Listen fist daemon for all servers maneger
-.then(() => new Promise<void>(resolve => daemon.Server.listen(5001, resolve))).then(() => console.log("Daemon Socket.io listen in port 5001"))
-.then(newDaemon.listen)
+.then(() => new Promise<void>(resolve => daemon.Server.listen(5000, resolve))).then(() => console.log("Daemon Socket.io listen in port 5001"))
 // Listen API for users
 .then(() => new Promise<void>(resolve => API.Server.listen(3000, resolve))).then(() => console.info("API listen in port 3000"))
 // Show is OK in console
